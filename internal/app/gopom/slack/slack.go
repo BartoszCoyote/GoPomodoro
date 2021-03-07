0ed226388cbe4a2b7236d47e3540409462682910
Michal Franc <michal.franc@form3.tech>
2021-03-07 12:34:21 +0000

    end snoozing functionality
    Co-authored-by: Michal Franc <michal.franc@form3.tech>

--- internal/app/gopom/slack/slack.go
+++ internal/app/gopom/slack/slack.go
@@ -49,5 +49,38 @@ func SetDnd(durationMinutes int) {
 }
 
 func EndDnd() {
-	fmt.Println("ending dnd on slack")
+	slackToken := os.Getenv("SLACK_TOKEN")
+	fmt.Println("SetDnd")
+
+	urlS := fmt.Sprintf("https://slack.com/api/dnd.endSnooze")
+
+	form := url.Values{}
+	form.Add("token", slackToken)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", urlS, strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bodyString := string(bodyBytes)
+		fmt.Println(bodyString)
+	}
+
+	if err != nil {
+		fmt.Println("Error when calling slack dnd.")
+	}
+
+	fmt.Println("starting end dnd on slack")
 }
