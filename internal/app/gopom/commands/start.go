a417c584b22313ffabe9fa79ac0947fb34fcdc5f
tbielaszewski <tbielaszewski@objectivity.co.uk>
2021-03-08 00:12:04 +0100

    setting custom volume or muting sounds

--- internal/app/gopom/commands/start.go
+++ internal/app/gopom/commands/start.go
@@ -7,15 +7,44 @@ import (
 )
 
 func init() {
+	startCmd.Flags().Float64VarP(&workVolume, "work-volume", "w", 80, "Sets volume of sound when working. Values 0..100.")
+	startCmd.Flags().Float64VarP(&finishVolume, "finish-volume", "f", 80, "Sets volume of sound when finished working. Values 0..100.")
+	startCmd.Flags().BoolVar(&muteWorkSounds, "mute-work", false, "Disables sound played when working.")
+	startCmd.Flags().BoolVar(&muteFinishSounds, "mute-finish", false, "Disables sound played when finished.")
 	rootCmd.AddCommand(startCmd)
 }
+
+var workVolume float64
+var finishVolume float64
+
+var muteWorkSounds bool
+var muteFinishSounds bool
 
 var startCmd = &cobra.Command{
 	Use:   "start [taskName]",
 	Short: "Start a task",
 	Run: func(cmd *cobra.Command, args []string) {
+		if muteWorkSounds {
+			workVolume = 0
+		}
+		if muteFinishSounds {
+			finishVolume = 0
+		}
+
+		//converting from human readable 0..100 range to Players -8..2 range
+		workVolume = workVolume/10 - 8
+		finishVolume = finishVolume/10 - 8
+
 		taskName := getTaskName(args)
-		pomodoro.NewPomodoro(taskName, 25*60, 5*60, 20*60, 4).Start()
+		pomodoro.NewPomodoro(&pomodoro.PomodoroSettings{
+			TaskName:          taskName,
+			WorkDuration:      25 * 60,
+			RestDuration:      5 * 60,
+			LongRestDuration:  20 * 60,
+			Cycles:            4,
+			WorkSoundVolume:   workVolume,
+			FinishSoundVolume: finishVolume,
+		}).Start()
 	},
 }
 
--- internal/app/gopom/sound/player.go
+++ internal/app/gopom/sound/player.go
@@ -2,11 +2,12 @@ package sound
 
 import (
 	"fmt"
+	"github.com/faiface/beep/effects"
 	"log"
 	"sync"
 	"time"
 
-	// required for statik file system to enable embeeded resources
+	// required for statik file system to enable embedded resources
 	_ "github.com/BartoszCoyote/GoPomodoro/internal/app/gopom/sound/soundpack"
 
 	"github.com/faiface/beep"
@@ -21,24 +22,32 @@ var (
 )
 
 type Player struct {
-	sound    beep.StreamSeekCloser
+	sound    *beep.StreamSeekCloser
+	volume   float64
+	mute     bool
 	stopChan chan struct{}
 }
 
-func NewPlayer(soundFile string) *Player {
+// Volume should be in range of -8..2 where 0 is unchanged system volume, -8 is barely audible, 2 is loud but not yet
+// distorted much. Setting volume higher than 2 may be unpleasant
+func NewPlayer(soundFile string, soundVolume float64) *Player {
+	sound, _ := loadSound(soundFile)
+
 	return &Player{
-		loadSound(soundFile),
+		sound,
+		soundVolume,
+		soundVolume <= -8,
 		make(chan struct{}),
 	}
 }
 
 func (p *Player) PlayLoop() {
-	controlledSound := &beep.Ctrl{Streamer: beep.Loop(-1, p.sound), Paused: false}
-	speaker.Play(controlledSound)
+	volume := p.withVolumeEffect(beep.Loop(-1, *p.sound))
+	speaker.Play(volume)
 
 	<-p.stopChan
 	speaker.Lock()
-	controlledSound.Paused = true
+	volume.Streamer = beep.Loop(0, *p.sound)
 	speaker.Unlock()
 }
 
@@ -48,14 +57,13 @@ func (p *Player) Stop() {
 
 func (p *Player) Play() {
 	done := make(chan struct{})
-	speaker.Play(beep.Seq(p.sound, beep.Callback(func() {
+	speaker.Play(beep.Seq(p.withVolumeEffect(beep.Loop(1, *p.sound)), beep.Callback(func() {
 		close(done)
 	})))
 	<-done
 }
 
-func loadSound(soundFile string) beep.StreamSeekCloser {
-
+func loadSound(soundFile string) (*beep.StreamSeekCloser, beep.Format) {
 	statikFS, err := fs.New()
 	if err != nil {
 		log.Fatal(err)
@@ -72,7 +80,7 @@ func loadSound(soundFile string) beep.StreamSeekCloser {
 	}
 	initSpeaker(format)
 
-	return streamer
+	return &streamer, format
 }
 
 func initSpeaker(format beep.Format) {
@@ -86,4 +94,8 @@ func initSpeaker(format beep.Format) {
 		}
 		speakerInitialized = true
 	}
+}
+
+func (p *Player) withVolumeEffect(streamer beep.Streamer) *effects.Volume {
+	return &effects.Volume{Streamer: streamer, Base: 2, Silent: p.mute, Volume: p.volume}
 }
