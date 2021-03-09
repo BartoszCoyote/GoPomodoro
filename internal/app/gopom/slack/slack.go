f5d1e5f0aaca116b7974c4c7e312b7b6aa34e82a
Michal Franc <michal.franc@form3.tech>
2021-03-09 10:34:36 +0000

    refactored the code to me more concise
    also added better error handling and more sane log lines
    
    Co-authored-by: Michal Franc <michal.franc@form3.tech>

--- internal/app/gopom/slack/slack.go
+++ internal/app/gopom/slack/slack.go
@@ -11,76 +11,76 @@ import (
 	"strings"
 )
 
-func SetDnd(durationMinutes int) {
+var TOKEN = getSlackToken()
+
+func getSlackToken() string {
 	slackToken := os.Getenv("SLACK_TOKEN")
-	fmt.Println("SetDnd")
+	if slackToken == "" {
+		log.Fatal("Empty slack token - please provide valid SLACK_TOKEN")
+	}
+	return slackToken
+}
+
+func callSlack(urlS string) error {
+	form := url.Values{}
+	form.Add("token", TOKEN)
+
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", urlS, strings.NewReader(form.Encode()))
+
+	if err != nil {
+		return fmt.Errorf("error when creating request to slack endpoint - %s", err)
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
+
+	resp, err := client.Do(req)
+
+	if resp != nil {
+		defer func() {
+			if err := resp.Body.Close(); err != nil {
+				err = fmt.Errorf("error when closing response body - %s", err)
+			}
+		}()
+	}
+
+	if err != nil {
+		return fmt.Errorf("error when calling slack endpoint - %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("error when reading body - %s", err)
+		}
+		return fmt.Errorf("slack responded with error message - %s", bodyBytes)
+	}
+
+	// return empty error
+	return nil
+}
+
+func SetDnd(durationMinutes int) {
+	log.Println("Setting DND on slack.")
 
 	urlS := fmt.Sprintf("https://slack.com/api/dnd.setSnooze?num_minutes=%d", durationMinutes)
 
-	form := url.Values{}
-	form.Add("token", slackToken)
-
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", urlS, strings.NewReader(form.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
-
+	err := callSlack(urlS)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyString := string(bodyBytes)
-		fmt.Println(bodyString)
-	}
-
-	if err != nil {
-		fmt.Println("Error when calling slack dnd.")
-	}
-
-	fmt.Println("starting dnd on slack")
+	log.Println("DND set on slack.")
 }
 
 func EndDnd() {
-	slackToken := os.Getenv("SLACK_TOKEN")
-	fmt.Println("SetDnd")
+	log.Println("Ending DND on slack.")
 
 	urlS := fmt.Sprintf("https://slack.com/api/dnd.endSnooze")
 
-	form := url.Values{}
-	form.Add("token", slackToken)
-
-	client := &http.Client{}
-	req, err := http.NewRequest("POST", urlS, strings.NewReader(form.Encode()))
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	req.Header.Add("Content-Length", strconv.Itoa(len(form.Encode())))
-
+	err := callSlack(urlS)
 	if err != nil {
-		fmt.Println(err)
+
 	}
-
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		bodyBytes, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		bodyString := string(bodyBytes)
-		fmt.Println(bodyString)
-	}
-
-	if err != nil {
-		fmt.Println("Error when calling slack dnd.")
-	}
-
-	fmt.Println("starting end dnd on slack")
 }
